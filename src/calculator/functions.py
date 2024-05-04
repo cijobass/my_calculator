919d64c0bc2a634308612cd72cc57ff27ba6621a
Tsubasa_Sakai <kaunijazzbass.1024@outlook.jp>
2024-05-04 13:13:04 +0900

    main.pyを他のファイルに分割

---
+++ src/calculator/functions.py
@@ -0,0 +1,23 @@
+import sympy
+
+def evaluate_expression(expr):
+    """Evaluate the mathematical expression using sympy with custom local variables."""
+    if not expr:
+        return ""  # Return empty string if expression is empty
+    try:
+        evaluated_expr = sympy.sympify(
+            expr,
+            locals={
+                'π': sympy.pi,
+                'e': sympy.E,
+                'factorial': sympy.factorial,
+                'ln': sympy.log,  # Natural logarithm (base e)
+                'log': lambda x: sympy.log(x, 10)  # Common logarithm (base 10)
+            }
+        )
+        if isinstance(evaluated_expr, sympy.Integer):
+            return str(int(evaluated_expr))
+        else:
+            return str(float(evaluated_expr))
+    except Exception as exc:
+        return "Error: " + str(exc)
